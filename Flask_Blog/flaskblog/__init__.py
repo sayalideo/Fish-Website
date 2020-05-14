71ce904635bf0706a222d58ba6398175fcdb2855
Sayali <sayalideo137@gmail.com>
2020-05-14 18:43:44 +0530

    Slider complete, navbar done

---
+++ Flask_Blog/flaskblog/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '48b95e763384fd9a92ff9bd0111f0b18'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+from flaskblog import routes
---
+++ Flask_Blog/flaskblog/forms.py
@@ -0,0 +1,55 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from wtforms.fields.html5 import EmailField
+from flaskblog.models import User
+from flask_login import current_user
+
+class RegistrationForm(FlaskForm):
+    username         = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)]) # DataRequired means it cant be empty
+    email            = EmailField('Email', validators=[DataRequired(), Email()])
+    password         = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit           = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username = username.data).first()
+        if user:
+            raise ValidationError('User Already Exists!')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email = email.data).first()
+        if user:
+            raise ValidationError('Email Already Exists!')
+
+class UpdateForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)]) # DataRequired means it cant be empty
+    email    = EmailField('Email', validators=[DataRequired(), Email()])
+    picture  = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
+    submit   = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username = username.data).first()
+            if user :
+                raise ValidationError('User Already Exists!')
+        
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email = email.data).first()
+            if user:
+                raise ValidationError('Email Already Exists!')
+
+class LoginForm(FlaskForm):
+    email    = EmailField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit   = SubmitField('Login')
+
+class PostForm(FlaskForm):
+    title   = StringField('Title', validators=[DataRequired()])
+    content = TextAreaField('Content', validators=[DataRequired()])
+    submit  = SubmitField('Post')
+
